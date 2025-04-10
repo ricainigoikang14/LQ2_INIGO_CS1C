ae52a912f36c13c371b95dc51e5af7e339487079
ricainigoikang14 <inigorica5@gmail.com>
2025-04-10 22:59:50 +0800

    Create L3.js

---
+++ L3.js
@@ -0,0 +1,11 @@
+let fname = "Jose Cruz";
+let i = 0;
+
+while (fname.length > i) {
+    let c = 2 + i;
+    let nLengthCounterTwo = fname.length - c;
+
+    console.log(fname);
+    console.log("Iterations Name was Printed: " + i);
+    i++;
+}
